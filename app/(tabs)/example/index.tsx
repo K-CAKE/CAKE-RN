0a23ae0f4d622f1a68d279fe7f2421af468ad2af
PROMLEE <donghoon099@naver.com>
2024-07-22 17:52:26 +0900

    chore: Update environment configuration and dependencies

--- app/(tabs)/example/index.tsx
+++ app/(tabs)/example/index.tsx
@@ -1,35 +1,83 @@
-import { useState, useEffect } from "react";
 import { supabase } from "@/hooks/supabase";
-import { View } from "react-native";
-import { Session } from "@supabase/supabase-js";
+import { Platform, View } from "react-native";
+import * as Linking from "expo-linking";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import { Button } from "@rneui/themed/dist/Button";
+import { makeRedirectUri } from "expo-auth-session";
+import * as WebBrowser from "expo-web-browser";
+import * as QueryParams from "expo-auth-session/build/QueryParams";
+import { useRecoilState } from "recoil";
+import { userid } from "@/atoms/userState";
 
+const redirectTo = makeRedirectUri();
+WebBrowser.maybeCompleteAuthSession();
 export default function App() {
-  const [session, setSession] = useState<Session | null>(null);
-  const [id, setId] = useState<string | null>(null);
+  const [useridState, setUseridState] = useRecoilState(userid);
 
-  useEffect(() => {
-    supabase.auth.getSession().then(({ data: { session } }) => {
-      setSession(session);
-    });
+  const Updatetoken = async () => {
+    const { error } = await supabase
+      .from("profiles")
+      .update({ username: "aaaa" })
+      .eq("id", useridState);
+    if (error) {
+      console.log(error);
+    } else {
+      console.log("success");
+    }
+  };
+  const createSessionFromUrl = async (url: string) => {
+    const { params, errorCode } = QueryParams.getQueryParams(url);
 
-    supabase.auth.onAuthStateChange((_event, session) => {
-      setSession(session);
+    if (errorCode) throw new Error(errorCode);
+    const { access_token, refresh_token } = params;
+
+    if (!access_token) return;
+
+    const { data, error } = await supabase.auth.setSession({
+      access_token,
+      refresh_token,
     });
-  }, []);
-  async function signInWithKakao() {
+    if (error) throw error;
+    setUseridState(data.user?.id ?? "");
+    setItem("access_token", data.session?.access_token ?? "");
+    setItem("refresh_token", data.session?.refresh_token ?? "");
+    return data.session;
+  };
+  async function setItem(key: string, value: string) {
+    if (Platform.OS === "web") {
+      return localStorage.setItem(key, value);
+    }
+    AsyncStorage.setItem(key, value);
+  }
+
+  const performOAuth = async () => {
     const { data, error } = await supabase.auth.signInWithOAuth({
       provider: "kakao",
+      options: {
+        redirectTo,
+        skipBrowserRedirect: true,
+      },
     });
-    setId(data.url);
-  }
-  async function signOut() {
-    const { error } = await supabase.auth.signOut();
-  }
+    if (error) throw error;
+
+    const res = await WebBrowser.openAuthSessionAsync(
+      data?.url ?? "",
+      redirectTo
+    );
+
+    if (res.type === "success") {
+      const { url } = res;
+      await createSessionFromUrl(url);
+    }
+  };
+
+  const url = Linking.useURL();
+  if (url) createSessionFromUrl(url);
   return (
     <View>
-      <Button onPress={signInWithKakao}>Sign in with Kakao</Button>
-      <Button onPress={signOut}>Sign out</Button>
+      <Button onPress={performOAuth}>Sign in with Kakao</Button>
+      {/* <Button onPress={signOut}>Sign out</Button> */}
+      <Button onPress={() => Updatetoken()} title="Update" />
     </View>
   );
 }
--- hooks/supabase.ts
+++ hooks/supabase.ts
@@ -1,35 +1,85 @@
-import AsyncStorage from "@react-native-async-storage/async-storage";
-import { createClient } from "@supabase/supabase-js";
-import { Platform } from "react-native";
 
-const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL || "";
-const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY || "";
-class SupabaseStorage {
+import 'react-native-url-polyfill/auto'
+import { createClient } from '@supabase/supabase-js'
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import * as SecureStore from 'expo-secure-store';
+import * as aesjs from 'aes-js';
+import 'react-native-get-random-values';
+import { Database } from '@/database.types';
+// import { Platform } from 'react-native';
+class LargeSecureStore {
+  private async _encrypt(key: string, value: string) {
+    const encryptionKey = crypto.getRandomValues(new Uint8Array(256 / 8))
+
+    const cipher = new aesjs.ModeOfOperation.ctr(encryptionKey, new aesjs.Counter(1))
+    const encryptedBytes = cipher.encrypt(aesjs.utils.utf8.toBytes(value))
+
+    await SecureStore.setItemAsync(key, aesjs.utils.hex.fromBytes(encryptionKey))
+
+    return aesjs.utils.hex.fromBytes(encryptedBytes)
+  }
+
+  private async _decrypt(key: string, value: string) {
+    const encryptionKeyHex = await SecureStore.getItemAsync(key)
+    if (!encryptionKeyHex) {
+      return encryptionKeyHex
+    }
+
+    const cipher = new aesjs.ModeOfOperation.ctr(
+      aesjs.utils.hex.toBytes(encryptionKeyHex),
+      new aesjs.Counter(1)
+    )
+    const decryptedBytes = cipher.decrypt(aesjs.utils.hex.toBytes(value))
+
+    return aesjs.utils.utf8.fromBytes(decryptedBytes)
+  }
+
   async getItem(key: string) {
-    if (Platform.OS === "web") {
-      if (typeof localStorage === "undefined") {
-        return null;
-      }
-      return localStorage.getItem(key);
+    const encrypted = await AsyncStorage.getItem(key)
+    if (!encrypted) {
+      return encrypted
     }
-    return AsyncStorage.getItem(key);
+
+    return await this._decrypt(key, encrypted)
   }
+
   async removeItem(key: string) {
-    if (Platform.OS === "web") {
-      return localStorage.removeItem(key);
-    }
-    return AsyncStorage.removeItem(key);
+    await AsyncStorage.removeItem(key)
+    await SecureStore.deleteItemAsync(key)
   }
+
   async setItem(key: string, value: string) {
-    if (Platform.OS === "web") {
-      return localStorage.setItem(key, value);
-    }
-    return AsyncStorage.setItem(key, value);
+    const encrypted = await this._encrypt(key, value)
+
+    await AsyncStorage.setItem(key, encrypted)
   }
 }
-export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+// class SupabaseStorage { // None Encryption(for web)
+//   async getItem(key: string) {
+//     if (Platform.OS === "web") {
+//       if (typeof localStorage === "undefined") {
+//         return null;
+//       }
+//       return localStorage.getItem(key);
+//     }
+//     return AsyncStorage.getItem(key);
+//   }
+//   async removeItem(key: string) {
+//     if (Platform.OS === "web") {
+//       return localStorage.removeItem(key);
+//     }
+//     return AsyncStorage.removeItem(key);
+//   }
+//   async setItem(key: string, value: string) {
+//     if (Platform.OS === "web") {
+//       return localStorage.setItem(key, value);
+//     }
+//     return AsyncStorage.setItem(key, value);
+//   }
+// }
+export const supabase = createClient<Database>(process.env.EXPO_PUBLIC_SUPABASE_URL || "",process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY||"", {
   auth: {
-    storage: new SupabaseStorage(),
+    storage: new LargeSecureStore(),
     autoRefreshToken: true,
     persistSession: true,
     detectSessionInUrl: false,
