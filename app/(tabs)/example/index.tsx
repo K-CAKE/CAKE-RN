175fd274b44941c7f30e57a6ef81dff0d8d542a7
PROMLEE <donghoon099@naver.com>
2024-08-13 16:22:11 +0900

    chore: Update npm dependencies and remove unused code

--- app/(tabs)/example/index.tsx
+++ app/(tabs)/example/index.tsx
@@ -1,24 +1,54 @@
 import { supabase } from '@/hooks/supabase';
-// import { Platform, TextInput, View, Text } from 'react-native';
-import { Platform } from 'react-native';
+import { Platform, TextInput, View, Text } from 'react-native';
+// import { Platform } from 'react-native';
 import * as Linking from 'expo-linking';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-// import { Button } from '@rneui/themed/dist/Button';
-// import { makeRedirectUri } from 'expo-auth-session';
+import { Button } from '@rneui/themed/dist/Button';
+import { makeRedirectUri } from 'expo-auth-session';
 import * as WebBrowser from 'expo-web-browser';
 import * as QueryParams from 'expo-auth-session/build/QueryParams';
-// import { useRecoilState } from 'recoil';
-// import { userid } from '@/atoms/userState';
+import { useRecoilState } from 'recoil';
+import { userid } from '@/atoms/userState';
 import { useEffect, useState } from 'react';
 import { NaverMapView } from '@mj-studio/react-native-naver-map';
-// const redirectTo = makeRedirectUri();
+import { GoogleSignin, GoogleSigninButton, statusCodes } from '@react-native-google-signin/google-signin';
+
+const redirectTo = makeRedirectUri();
 WebBrowser.maybeCompleteAuthSession();
 
 export default function App() {
-  // const [useridState] = useRecoilState(userid);
-  // const [chatting, setChatting] = useState('');
+  GoogleSignin.configure({
+    scopes: ['https://www.googleapis.com/auth/drive.readonly'],
+    webClientId: process.env.EXPO_PUBLIC_GOOGLE_CLIENT_ID,
+  });
+  const [useridState] = useRecoilState(userid);
+  const [chatting, setChatting] = useState('');
   const [chat, setChat] = useState<string[]>([]);
-
+  const signInWithGoogle = async () => {
+    try {
+      await GoogleSignin.hasPlayServices();
+      const userInfo = await GoogleSignin.signIn();
+      if (userInfo.idToken) {
+        const { data, error } = await supabase.auth.signInWithIdToken({
+          provider: 'google',
+          token: userInfo.idToken,
+        });
+        console.log(error, data);
+      } else {
+        throw new Error('no ID token present!');
+      }
+    } catch (error: any) {
+      if (error.code === statusCodes.SIGN_IN_CANCELLED) {
+        // user cancelled the login flow
+      } else if (error.code === statusCodes.IN_PROGRESS) {
+        // operation (e.g. sign in) is in progress already
+      } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
+        // play services not available or outdated
+      } else {
+        // some other error happened
+      }
+    }
+  };
   useEffect(() => {
     const fetchChat = async () => {
       const { data, error } = await supabase.from('chat').select('chatting');
@@ -31,28 +61,30 @@ export default function App() {
     fetchChat();
   }, []);
 
-  // const Updatetoken = async (text: string) => {
-  //   const { error } = await supabase.from('chat').insert({ chatting: text });
-  //   if (error) {
-  //     console.log(error);
-  //   } else {
-  //     // console.log('success');
-  //   }
-  // };
+  const Updatetoken = async (text: string) => {
+    const { error } = await supabase.from('chat').insert({ chatting: text });
+    if (error) {
+      console.log(error);
+    } else {
+      // console.log('success');
+    }
+  };
   const createSessionFromUrl = async (url: string) => {
     const { params, errorCode } = QueryParams.getQueryParams(url);
 
     if (errorCode) throw new Error(errorCode);
     const { access_token, refresh_token } = params;
 
-    if (!access_token) return;
+    if (!access_token) {
+      throw new Error('No access_token in URL');
+    }
 
     const { data, error } = await supabase.auth.setSession({
       access_token,
       refresh_token,
     });
     if (error) throw error;
-    console.log(data.user);
+    console.log(data);
     // setUseridState(data.user?.user_metadata);
     await setItem('access_token', data.session?.access_token ?? '');
     await setItem('refresh_token', data.session?.refresh_token ?? '');
@@ -65,29 +97,33 @@ export default function App() {
     await AsyncStorage.setItem(key, value);
   }
 
-  // const performOAuth = async () => {
-  //   const { data, error } = await supabase.auth.signInWithOAuth({
-  //     provider: 'kakao',
-  //     options: {
-  //       redirectTo,
-  //       skipBrowserRedirect: true,
-  //     },
-  //   });
-  //   if (error) throw error;
-
-  //   const res = await WebBrowser.openAuthSessionAsync(data?.url ?? '', redirectTo);
-
-  //   if (res.type === 'success') {
-  //     const { url } = res;
-  //     await createSessionFromUrl(url);
-  //   }
-  // };
-  // async function signOut() {
-  //   await supabase.auth.signOut();
-  //   // setUseridState('');
-  //   await AsyncStorage.removeItem('access_token');
-  //   await AsyncStorage.removeItem('refresh_token');
-  // }
+  const performOAuth = async () => {
+    const { data, error } = await supabase.auth.signInWithOAuth({
+      provider: 'google',
+      options: {
+        queryParams: {
+          access_type: 'offline',
+          prompt: 'consent',
+        },
+        redirectTo,
+        skipBrowserRedirect: true,
+      },
+    });
+    if (error) throw error;
+    console.log(data.url);
+    const res = await WebBrowser.openAuthSessionAsync(data?.url ?? '', redirectTo);
+    console.log(res);
+    if (res.type === 'success') {
+      const { url } = res;
+      await createSessionFromUrl(url);
+    }
+  };
+  async function signOut() {
+    await supabase.auth.signOut();
+    // setUseridState('');
+    await AsyncStorage.removeItem('access_token');
+    await AsyncStorage.removeItem('refresh_token');
+  }
 
   const handleInserts = (payload: any) => {
     setChat([...chat, payload.new.chatting]);
@@ -100,22 +136,22 @@ export default function App() {
   const url = Linking.useURL();
   if (url) createSessionFromUrl(url);
   return (
-    <NaverMapView style={{ flex: 1 }} />
-    // <View>
-    //   <Button onPress={performOAuth}>Sign in with Kakao</Button>
-    //   <Button onPress={signOut}>Sign out</Button>
-    //   {chat.map((item, index) => (
-    //     <Text key={index}>{item}</Text>
-    //   ))}
-    //   <View className={'mt-10'}>
-    //     <TextInput
-    //       onChangeText={(text) => {
-    //         setChatting(text);
-    //       }}
-    //       placeholder="아무거나 입력해주세요."
-    //     />
-    //     <Button onPress={() => Updatetoken(chatting)} title="Send" />
-    //   </View>
-    // </View>
+    // <NaverMapView style={{ flex: 1 }} />
+    <View>
+      <Button onPress={signInWithGoogle}>Sign in with Google</Button>
+      <Button onPress={signOut}>Sign out</Button>
+      {chat.map((item, index) => (
+        <Text key={index}>{item}</Text>
+      ))}
+      <View className={'mt-10'}>
+        <TextInput
+          onChangeText={(text) => {
+            setChatting(text);
+          }}
+          placeholder="아무거나 입력해주세요."
+        />
+        <Button onPress={() => Updatetoken(chatting)} title="Send" />
+      </View>
+    </View>
   );
 }
