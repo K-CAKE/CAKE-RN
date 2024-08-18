db055a5efd08c783e944de1574ba1b272b8341e8
PROMLEE <donghoon099@naver.com>
2024-08-18 21:33:33 +0900

    feat: 홈 화면 구글 로그인 추가

--- app/(tabs)/example/index.tsx
+++ app/(tabs)/example/index.tsx
@@ -3,7 +3,7 @@ import { Platform, TextInput, View, Text } from 'react-native';
 // import { Platform } from 'react-native';
 import * as Linking from 'expo-linking';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import { Button } from '@rneui/themed/dist/Button';
+// import { Button } from '@rneui/themed/dist/Button';
 import { makeRedirectUri } from 'expo-auth-session';
 import * as WebBrowser from 'expo-web-browser';
 import * as QueryParams from 'expo-auth-session/build/QueryParams';
@@ -12,44 +12,44 @@ import { userid } from '@/atoms/userState';
 import { useEffect, useState } from 'react';
 import { Button } from 'react-native-paper';
 import { NaverMapView } from '@mj-studio/react-native-naver-map';
-import { GoogleSignin, GoogleSigninButton, statusCodes } from '@react-native-google-signin/google-signin';
+// import { GoogleSignin, GoogleSigninButton, statusCodes } from '@react-native-google-signin/google-signin';
 
 const redirectTo = makeRedirectUri();
 WebBrowser.maybeCompleteAuthSession();
 
 export default function App() {
-  GoogleSignin.configure({
-    scopes: ['https://www.googleapis.com/auth/drive.readonly'],
-    webClientId: process.env.EXPO_PUBLIC_GOOGLE_CLIENT_ID,
-  });
+  // GoogleSignin.configure({
+  //   scopes: ['https://www.googleapis.com/auth/drive.readonly'],
+  //   webClientId: process.env.EXPO_PUBLIC_GOOGLE_CLIENT_ID,
+  // });
   const [useridState] = useRecoilState(userid);
   const [chatting, setChatting] = useState('');
   const [chat, setChat] = useState<string[]>([]);
-  const signInWithGoogle = async () => {
-    try {
-      await GoogleSignin.hasPlayServices();
-      const userInfo = await GoogleSignin.signIn();
-      if (userInfo.idToken) {
-        const { data, error } = await supabase.auth.signInWithIdToken({
-          provider: 'google',
-          token: userInfo.idToken,
-        });
-        console.log(error, data);
-      } else {
-        throw new Error('no ID token present!');
-      }
-    } catch (error: any) {
-      if (error.code === statusCodes.SIGN_IN_CANCELLED) {
-        // user cancelled the login flow
-      } else if (error.code === statusCodes.IN_PROGRESS) {
-        // operation (e.g. sign in) is in progress already
-      } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
-        // play services not available or outdated
-      } else {
-        // some other error happened
-      }
-    }
-  };
+  // const signInWithGoogle = async () => {
+  //   try {
+  //     await GoogleSignin.hasPlayServices();
+  //     const userInfo = await GoogleSignin.signIn();
+  //     if (userInfo.idToken) {
+  //       const { data, error } = await supabase.auth.signInWithIdToken({
+  //         provider: 'google',
+  //         token: userInfo.idToken,
+  //       });
+  //       console.log(error, data);
+  //     } else {
+  //       throw new Error('no ID token present!');
+  //     }
+  //   } catch (error: any) {
+  //     if (error.code === statusCodes.SIGN_IN_CANCELLED) {
+  //       // user cancelled the login flow
+  //     } else if (error.code === statusCodes.IN_PROGRESS) {
+  //       // operation (e.g. sign in) is in progress already
+  //     } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
+  //       // play services not available or outdated
+  //     } else {
+  //       // some other error happened
+  //     }
+  //   }
+  // };
   useEffect(() => {
     const fetchChat = async () => {
       const { data, error } = await supabase.from('chat').select('chatting');
--- app/Login.tsx
+++ app/Login.tsx
@@ -1,11 +1,71 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { Text, View, TouchableOpacity, Image, StyleSheet, Dimensions } from 'react-native';
+import { Platform, Text, View, TouchableOpacity, Image, StyleSheet, Dimensions } from 'react-native';
 import { router } from 'expo-router';
+import { supabase } from '@/hooks/supabase';
+import { makeRedirectUri } from 'expo-auth-session';
+import * as WebBrowser from 'expo-web-browser';
+import * as Linking from 'expo-linking';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import * as QueryParams from 'expo-auth-session/build/QueryParams';
 
 const { width, height } = Dimensions.get('window');
 
+const redirectTo = makeRedirectUri();
+WebBrowser.maybeCompleteAuthSession();
 export default function Login() {
+  const performOAuth = async () => {
+    const { data, error } = await supabase.auth.signInWithOAuth({
+      provider: 'google',
+      options: {
+        queryParams: {
+          access_type: 'offline',
+          prompt: 'consent',
+        },
+        redirectTo,
+        skipBrowserRedirect: true,
+      },
+    });
+    if (error) throw error;
+    // console.log(data.url);
+    const res = await WebBrowser.openAuthSessionAsync(data?.url ?? '', redirectTo);
+
+    // console.log(res);
+    if (res.type === 'success') {
+      const { url } = res;
+      await createSessionFromUrl(url);
+    }
+    router.push('/(tabs)/home');
+  };
+  async function setItem(key: string, value: string) {
+    if (Platform.OS === 'web') {
+      return localStorage.setItem(key, value);
+    }
+    await AsyncStorage.setItem(key, value);
+  }
+  const createSessionFromUrl = async (url: string) => {
+    const { params, errorCode } = QueryParams.getQueryParams(url);
+    if (errorCode) throw new Error(errorCode);
+    console.log(params);
+    const { access_token, refresh_token } = params;
+
+    if (!access_token) {
+      throw new Error('No access_token in URL');
+    }
+
+    const { data, error } = await supabase.auth.setSession({
+      access_token,
+      refresh_token,
+    });
+    if (error) throw error;
+    await setItem('access_token', data.session?.access_token ?? '');
+    await setItem('refresh_token', data.session?.refresh_token ?? '');
+    return data.session;
+  };
+
+  const url = Linking.useURL();
+  if (url) createSessionFromUrl(url);
+
   return (
     <View style={styles.container}>
       <StatusBar style="light" />
@@ -26,7 +86,7 @@ export default function Login() {
       <TouchableOpacity
         style={styles.googleLoginButton}
         onPress={() => {
-          /* Google login logic */
+          performOAuth();
         }}
       >
         <Image source={require('../assets/images/google_logo.png')} style={styles.googleLogo} />
