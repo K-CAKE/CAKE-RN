7d44b66bbcb0d5004b0101e0c3ffa10ba910a3c1
PROMLEE <donghoon099@naver.com>
2024-08-25 15:06:51 +0900

    feat: 채팅 완료

--- app/(tabs)/chat/[id].tsx
+++ app/(tabs)/chat/[id].tsx
@@ -8,38 +8,58 @@ import { Stack, useLocalSearchParams, useNavigation } from 'expo-router';
 import { Ionicons } from '@expo/vector-icons';
 import FontAwesome from '@expo/vector-icons/FontAwesome';
 import { FlatList } from 'react-native-gesture-handler';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import React from 'react';
+import { useRecoilState } from 'recoil';
+import { userState } from '@/atoms/userState';
 
 WebBrowser.maybeCompleteAuthSession();
 
 export default function App() {
-  // const [useridState] = useRecoilState(userid);
+  const insets = useSafeAreaInsets();
+  const [useridState] = useRecoilState(userState);
   const { id } = useLocalSearchParams();
   const [chatting, setChatting] = useState('');
-  const [chat, setChat] = useState<string[]>([]);
+  const [chat, setChat] = useState<any>([]);
+  const [isUser1, setIsUser1] = useState(true);
+  const [otherUser, setOtheruser] = useState<number | null | undefined>(0);
   useEffect(() => {
     const fetchChat = async () => {
-      const { data, error } = await supabase.from('chat').select('chatting');
-      if (error) {
+      const { data, error } = await supabase.from('chat').select('chatting, is_user1').eq('chat_room', id);
+      const { data: roomdata, error: roomerror } = await supabase
+        .from('chat_room')
+        .select('user1, user2')
+        .eq('id', id)
+        .maybeSingle();
+      setIsUser1(useridState.id === roomdata?.user1);
+      setOtheruser(roomdata?.user2);
+      if (error || roomerror) {
         console.log(error);
       } else {
-        setChat(data.map((item) => item.chatting?.toString() ?? ''));
+        setChat(
+          data.map((item, idx) => ({
+            chatting: item.chatting?.toString() || '',
+            user: useridState.id === roomdata?.user1 && item.is_user1 ? 'me' : 'user' + roomdata?.user2,
+          })),
+        );
       }
     };
     fetchChat();
-  }, []);
+  }, [id, useridState.id]);
 
   const Updatetoken = async (text: string) => {
-    const { error } = await supabase.from('chat').insert({ chatting: text });
+    const { error } = await supabase.from('chat').insert({ chatting: text, is_user1: isUser1, chat_room: Number(id) });
     if (error) {
       console.log(error);
     } else {
-      // console.log('success');
+      setChat([...chat, { chatting, user: 'me' }]);
+      setChatting('');
     }
   };
 
   const handleInserts = (payload: any) => {
-    setChat([...chat, payload.new.chatting]);
-    console.log('Change received!', payload);
+    setChat([...chat, { chatting: payload.new.chatting, user: 'user2' }]);
+    // console.log('Change received!', payload);
   };
 
   supabase
@@ -50,20 +70,32 @@ export default function App() {
 
   const renderItem = () => {
     return (
-      <View>
-        {chat.map((item, index) => (
-          <Text key={index}>{item}</Text>
-        ))}
-        <View style={{ flexDirection: 'row' }}>
-          <TextInput
-            onChangeText={(text) => setChatting(text)}
-            placeholder="아무거나 입력해주세요."
-            className="w-3/4 px-3 border border-gray-300 mx-1"
-          />
-          <Button onPress={() => Updatetoken(chatting)} icon="send" mode="contained" textColor="white">
-            send
-          </Button>
-        </View>
+      <View style={{ gap: 4 }}>
+        {chat.length ? (
+          chat.map((item: any, index: number) => (
+            <View key={index} className="mx-3">
+              {item.user === 'me' ? (
+                <>
+                  <Text className="ml-auto">me</Text>
+                  <View className="mx-2 bg-red-500 p-2 text-xl rounded-md ml-auto w-auto max-w-40">
+                    <Text className="text-white">{item.chatting}</Text>
+                  </View>
+                </>
+              ) : (
+                <>
+                  <Text>{item.user}</Text>
+                  <View className="mx-2 bg-red-200 p-2 text-xl rounded-md mr-auto w-auto max-w-40">
+                    <Text>{item.chatting}</Text>
+                  </View>
+                </>
+              )}
+            </View>
+          ))
+        ) : (
+          <View className="items-center">
+            <Text>{'no chat'}</Text>
+          </View>
+        )}
       </View>
     );
   };
@@ -71,7 +103,7 @@ export default function App() {
     <LinearGradient colors={['#ffd4d1', '#efefef']} locations={[0.0, 0.5]} style={{ flex: 1 }}>
       <Stack.Screen
         options={{
-          headerTitle: 'Chat With ' + id,
+          headerTitle: 'Chat With User' + otherUser,
           headerTitleAlign: 'center',
           headerBackVisible: false,
           headerTransparent: true,
@@ -111,12 +143,38 @@ export default function App() {
 
       <View>
         <FlatList
-          style={{ paddingTop: 120 }}
+          style={{ paddingTop: 140 }}
           data={['']}
           renderItem={renderItem}
-          // keyExtractor={(item) => item.key}
-          ListFooterComponent={<View style={{ height: 300 }} />}
+          inverted
+          ListFooterComponent={<View style={{ height: 250 }} />}
         />
+      </View>
+      <View
+        style={{
+          position: 'absolute',
+          bottom: 0,
+          width: '100%',
+          height: 130,
+          backgroundColor: 'white',
+        }}
+      />
+      <View
+        style={{
+          flexDirection: 'row',
+          position: 'absolute',
+          bottom: 80 - insets.bottom,
+        }}
+      >
+        <TextInput
+          value={chatting}
+          onChangeText={(text) => setChatting(text)}
+          placeholder="아무거나 입력해주세요."
+          className="w-3/4 px-3 border border-gray-300 mx-1"
+        />
+        <Button onPress={() => Updatetoken(chatting)} icon="send" mode="contained" textColor="white">
+          send
+        </Button>
       </View>
     </LinearGradient>
   );
