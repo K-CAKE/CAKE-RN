e9d1f19a0b333f7021ccee2bf6ad1eb322ed3c33
seulhuioh <mycindy0710@cau.ac.kr>
2024-08-25 14:42:15 +0900

    feat:현재위치 위도 경도 값 받아오기

--- app/(tabs)/delivery/DeliveryMap.tsx
+++ app/(tabs)/delivery/DeliveryMap.tsx
@@ -1,16 +1,46 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useEffect, useState } from "react";
+import { View, Text, StyleSheet } from "react-native";
+import * as Location from 'expo-location';
 
 export default function DeliveryMap() {
-  <View style={styles.block}>
-    <Text>DeliveryMap</Text>
-  </View>;
+  const [location, setLocation] = useState<Location.LocationObject | null>(null);
+  const [errorMsg, setErrorMsg] = useState<string | null>(null);
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location was denied');
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    })();
+  }, []);
+
+  let text = 'Waiting..';
+  if (errorMsg) {
+    text = errorMsg;
+  } else if (location) {
+    text = JSON.stringify(location);
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.paragraph}>{text}</Text>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-  block: {
+  container: {
     flex: 1,
-    alignItems: 'center',
     justifyContent: 'center',
+    paddingHorizontal: 20,
+  },
+  paragraph: {
+    fontSize: 18,
+    textAlign: 'center',
   },
 });
